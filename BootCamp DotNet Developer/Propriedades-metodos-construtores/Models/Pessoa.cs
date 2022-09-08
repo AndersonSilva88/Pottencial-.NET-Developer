391b24e2b72b31e4e716de994ad4dfc277cb3a83
Anderson Oliveira da Silva <anderson.os88@hotmail.com>
2022-09-08 12:12:34 -0300

    tupla, desconstrutor

--- BootCamp DotNet Developer/Propriedades-metodos-construtores/Models/Pessoa.cs
+++ BootCamp DotNet Developer/Propriedades-metodos-construtores/Models/Pessoa.cs
@@ -18,6 +18,12 @@ namespace Propriedades_metodos_construtores.Models
             Sobrenome = sobrenome;
         }
 
+        public void Deconstruct(out string nome, out string sobrenome)
+        {
+            nome = Nome;
+            sobrenome = Sobrenome;
+        }
+
         private string _nome;
         private int _idade;
         public string Nome
--- BootCamp DotNet Developer/Propriedades-metodos-construtores/Program.cs
+++ BootCamp DotNet Developer/Propriedades-metodos-construtores/Program.cs
@@ -1,23 +1,74 @@
 ﻿using Propriedades_metodos_construtores.Models;
 using System.Globalization;
 
-Dictionary<string, string> estados = new Dictionary<string, string>();
+int numero = 10;
+bool ehPar = false;
 
-estados.Add("PR", "Paraná");
-estados.Add("SP", "São Paulo");
-estados.Add("MG", "Minas Gerais");
+ehPar = numero % 2 == 0;
+Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "impar"));
 
-foreach (KeyValuePair<string, string> item in estados)
-{
-    Console.WriteLine($"Chave: {item.Key}, valor: {item.Value}");
-}
-Console.WriteLine("**** Removendo ******");
-estados.Remove("SP");
+// if (numero % 2 == 0)
+// {
+//     Console.WriteLine($"O Número {numero} é par");
+// }
+// else
+// {
+//     Console.WriteLine($"O Número é impar");
+// }
 
-foreach (KeyValuePair<string, string> item in estados)
-{
-    Console.WriteLine($"Chave: {item.Key}, valor: {item.Value}");
-}
+
+
+// Pessoa pessoa = new Pessoa("Anderson", "Silva");
+
+// (string nome, string sobrenome) = pessoa;
+
+// Console.WriteLine($"{nome} {sobrenome}");
+
+
+// LeituraArquivo arquivo = new LeituraArquivo();
+
+// var (sucesso, linhasArquivo, quantidadeLinhas) = arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
+
+// if (sucesso)
+// {
+//     Console.WriteLine("Quantidade linhas do arquivo: " + quantidadeLinhas);
+//     foreach (string linha in linhasArquivo)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Não foi possivel ler o arquivo");
+// }
+
+
+
+// (int Id, string Nome, string Sobrenome) tupla = (1, "Anderson", "Silva");
+
+
+// Console.WriteLine($"Id: {tupla.Id}");
+// Console.WriteLine($"Nome: {tupla.Nome}");
+// Console.WriteLine($"Sobrenome: {tupla.Sobrenome}");
+
+
+// Dictionary<string, string> estados = new Dictionary<string, string>();
+
+// estados.Add("PR", "Paraná");
+// estados.Add("SP", "São Paulo");
+// estados.Add("MG", "Minas Gerais");
+
+// foreach (KeyValuePair<string, string> item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, valor: {item.Value}");
+// }
+// Console.WriteLine("**** Removendo ******");
+// estados.Remove("SP");
+
+// foreach (KeyValuePair<string, string> item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, valor: {item.Value}");
+// }
 
 // Stack<int> pilha = new Stack<int>();
 // Console.WriteLine("**** Adicionando elementos na pilha ****");
