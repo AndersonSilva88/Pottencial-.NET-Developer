85d22e5dfd245a95465d8651b512a8baa445f63b
Anderson Oliveira da Silva <anderson.os88@hotmail.com>
2022-09-10 17:42:22 -0300

    project part one created class

---
+++ BootCamp DotNet Developer/Projeto-Sistema-De-Hospedagem/src/Models/Pessoa.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Sistema_De_Hospedagem.src.Models
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+
+        }
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string NomeCompleto { get; set; }
+    }
+}
