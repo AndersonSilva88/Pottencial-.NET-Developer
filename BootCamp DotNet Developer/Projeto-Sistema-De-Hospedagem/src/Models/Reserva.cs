443ff60faead10d05927ac11436296c113038182
Anderson Oliveira da Silva <anderson.os88@hotmail.com>
2022-09-12 13:31:46 -0300

    projeto hospedagem

--- BootCamp DotNet Developer/Projeto-Sistema-De-Hospedagem/src/Models/Reserva.cs
+++ BootCamp DotNet Developer/Projeto-Sistema-De-Hospedagem/src/Models/Reserva.cs
@@ -22,16 +22,13 @@ namespace Projeto_Sistema_De_Hospedagem.src.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
+
             if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
                 throw new Exception("A Quantidade de hóspedes é maior que a capacidade da suite!!");
             }
         }
@@ -42,20 +39,13 @@ namespace Projeto_Sistema_De_Hospedagem.src.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
             return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (DiasReservados >= 10)
             {
                 decimal Calcular = DiasReservados * Suite.ValorDiaria;
