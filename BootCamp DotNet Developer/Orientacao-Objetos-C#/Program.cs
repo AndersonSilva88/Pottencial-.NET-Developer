09a39b1f233721db6e96bc7f459743af37eedec1
Anderson Oliveira da Silva <anderson.os88@hotmail.com>
2022-09-13 10:11:38 -0300

    Heranca e polimorfismo

--- BootCamp DotNet Developer/Orientacao-Objetos-C#/Program.cs
+++ BootCamp DotNet Developer/Orientacao-Objetos-C#/Program.cs
@@ -1,11 +1,25 @@
 ﻿using Orientacao_Objetos_C_.Models;
 
-ContaCorrente cc = new ContaCorrente(123, 1000);
 
-cc.ExibirSaldo();
-cc.Sacar(1500);
-cc.ExibirSaldo();
+Aluno aluno = new Aluno();
 
+aluno.Nome = "Anderson";
+aluno.Idade = 20;
+aluno.Email = "anderson@email.com";
+aluno.Nota = 10;
+aluno.Apresentar();
+
+Professor professor = new Professor();
+professor.Nome = "CleosWaldo";
+professor.Idade = 45;
+professor.Salario = 1000;
+professor.Apresentar();
+
+// ContaCorrente cc = new ContaCorrente(123, 1000);
+
+// cc.ExibirSaldo();
+// cc.Sacar(1500);
+// cc.ExibirSaldo();
 
 
 // Pessoa pessoa = new Pessoa();
